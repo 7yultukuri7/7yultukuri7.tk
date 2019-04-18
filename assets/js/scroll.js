33e0f44d48b1bed60d532e65ee00724e53ca450f
7yultukuri7 <49602287+7yultukuri7@users.noreply.github.com>
2019-04-18 23:01:48 +0900

    Create scroll.js

---
+++ assets/js/scroll.js
@@ -0,0 +1,14 @@
+const btn = $('.scroll-button');
+
+$(window).scroll(() => {
+  if ($(this).scrollTop() > 500) {
+    btn.fadeIn();
+  } else {
+    btn.fadeOut();
+  }
+});
+
+btn.click ((e) => {
+  e.preventDefault(); // 重要
+  $('body, html').animate({ scrollTop: 0 }, 500);
+});
